9e1de6b04dd0f3e131c812b1b6afd64c7b3b4394
James Robinson <james@jlr-photo.com>
2019-04-19 09:35:47 -0400

    Implementation of prior_current_next() and tests.

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,36 +1,46 @@
-from jlr.utils import by_chunks
+from jlr.utils import by_chunks, prior_current_next
 
 
 
 def test_by_chunks():
 
-    def chunks_as_list(chunked_iter):
-        return list(chunked_iter)
+    assert list(by_chunks([], 3)) == [], list(by_chunks([], 3))
 
-    assert chunks_as_list(by_chunks([], 3)) == [], chunks_as_list(by_chunks([], 3))
+    assert list(by_chunks([1,2,], 3)) == [[1,2]]
 
-    assert chunks_as_list(by_chunks([1,2,], 3)) == [[1,2]]
+    assert list(by_chunks([1,2,3,], 3)) == [[1,2,3]]
 
-    assert chunks_as_list(by_chunks([1,2,3,], 3)) == [[1,2,3]]
+    assert list(by_chunks([1,2,3,4], 3)) == [[1,2,3], [4]]
 
-    assert chunks_as_list(by_chunks([1,2,3,4], 3)) == [[1,2,3], [4]]
+    assert list(by_chunks([1,2,3,4,5,6], 3)) == [[1,2,3], [4,5,6]]
 
-    assert chunks_as_list(by_chunks([1,2,3,4,5,6], 3)) == [[1,2,3], [4,5,6]]
-
-    assert chunks_as_list(by_chunks([1,2,3,4,5,6,7], 3)) == [[1,2,3], [4,5,6], [7,]]
+    assert list(by_chunks([1,2,3,4,5,6,7], 3)) == [[1,2,3], [4,5,6], [7,]]
 
     # Now test padding by working down ...
-    assert chunks_as_list(by_chunks([1,2,3,4,5,6,7], 3, pad_with=None)) == [[1,2,3], [4,5,6], [7,None,None]]
+    assert list(by_chunks([1,2,3,4,5,6,7], 3, pad_with=None)) == [[1,2,3], [4,5,6], [7,None,None]]
 
-    assert chunks_as_list(by_chunks([1,2,3,4,5,6], 3, pad_with=None)) == [[1,2,3], [4,5,6]]
+    assert list(by_chunks([1,2,3,4,5,6], 3, pad_with=None)) == [[1,2,3], [4,5,6]]
 
-    assert chunks_as_list(by_chunks([1,2,3,4,5], 3, pad_with=None)) == [[1,2,3], [4,5,None]]
+    assert list(by_chunks([1,2,3,4,5], 3, pad_with=None)) == [[1,2,3], [4,5,None]]
 
-    assert chunks_as_list(by_chunks([1,], 3, pad_with=None)) == [[1,None, None]]
+    assert list(by_chunks([1,], 3, pad_with=None)) == [[1,None, None]]
 
-    assert chunks_as_list(by_chunks([], 3, pad_with=None)) == []
+    assert list(by_chunks([], 3, pad_with=None)) == []
 
     # Things other than None can be used.
-    assert chunks_as_list(by_chunks([1,2,3,4,5], 3, pad_with='sdf')) == [[1,2,3], [4,5,'sdf']]
+    assert list(by_chunks([1,2,3,4,5], 3, pad_with='sdf')) == [[1,2,3], [4,5,'sdf']]
 
+def test_prior_current_next():
+
+    # Basic usage.
+    assert list(prior_current_next([1,2,3,4])) == [(None, 1, 2), (1,2,3), (2,3,4), (3,4,None)]
+
+    # Overriding the pad value...
+    assert list(prior_current_next([1,2,3,4], -1)) == [(-1, 1, 2), (1,2,3), (2,3,4), (3,4,-1)]
+
+    # Short input
+    assert list(prior_current_next([1,])) == [(None, 1, None)]
+
+    # Really short input
+    assert list(prior_current_next([])) == []
 
--- utils.py
+++ utils.py
@@ -1,3 +1,4 @@
+from itertools import tee, chain
 
 no_padding=object()
 def by_chunks(iterable, chunk_size, pad_with=no_padding):
@@ -19,3 +20,34 @@ def by_chunks(iterable, chunk_size, pad_with=no_padding):
             yield chunk
 
 
+def prior_current_next(iterable, pad=None):
+    """"
+        s -> (pad, s0, s1), (s0,s1,s2),  ..., (sN-1, sN, pad).
+
+        Aka 'prior, current, and next' elements in an iteration all
+        at once.
+
+        Lets you visit each element in an iterable while also being
+        presented with the prior and next element. At the beginning and
+        end of the iteration the prior and/or next element will
+        be presented as the 'pad' value.
+
+        Similar to using SQL windowing functions LAG and LEAD
+        at the same time.
+    """
+
+    # Get three iterables out of the one.
+    a, b, c = tee(iterable, 3)
+
+    # Hook up to drain the pad value once from the first iterable as
+    # the initial degenerate 'prior' value.
+    a = chain([pad], a)
+
+    # Likewise mirror image for the 'next' value.
+    c = chain(c, [pad])
+
+    # Must pre-consume from next before returning.
+    next(c, None)
+
+    # Return zipped wrapping of all three.
+    return zip(a, b, c)
